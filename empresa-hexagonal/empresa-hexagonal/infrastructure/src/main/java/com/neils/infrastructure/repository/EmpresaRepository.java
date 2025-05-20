0e14f837c589534a7391f20b5ab935606c9c90ff
starmgret814 <neilsilizarbe051@gmail.com>
2025-05-20 02:32:03 -0500

    Delete empresa-hexagonal/empresa-hexagonal directory

--- empresa-hexagonal/empresa-hexagonal/infrastructure/src/main/java/com/neils/infrastructure/repository/EmpresaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.neils.infrastructure.repository;
-
-import com.neils.infrastructure.entity.EmpresaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmpresaRepository extends JpaRepository<EmpresaEntity, String> {
-}
